47f1f7f71610c483e047c505d8de36a913e65337
Аушахманов Ильяс Жанатович <@corp.itstep>
2019-01-11 12:01:11 +0600

    first commit

---
+++ SMS/Authorisation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS
+{
+    public class Authorisation
+    {
+        string _login;
+         string _password;
+        string _email;
+        char _phone;
+        string _code;
+        public string GetLogin()
+        {
+            return _login;
+        }
+        public void SetLogin(string login)
+        {
+            _login = login;
+        }
+        public string GetPassword()
+        {
+            return _password;
+        }
+        public void SetPassword(string password)
+        {
+            _password = password;
+        }
+        public string GetEmail()
+        {
+            return _email;
+        }
+        public void SetEmail(string email)
+        {
+            _email = email;
+        }
+        public char GetPhone()
+        {
+            return _phone;
+        }
+        public void SetPhone(char phone)
+        {
+            _phone = phone;
+        }
+        public string GetCode()
+        {
+            return _code;
+        }
+        public void SetCode(string code)
+        {
+            _code = code;
+        }
+
+
+        public void LoginAndPassword()
+        {
+            Console.WriteLine("Введите логин");
+            _login = Console.ReadLine();
+            Console.Write("Введите пароль");
+            
+            
+            _password = Console.ReadLine();
+            Console.Write("Подтвердите пароль");
+            _password = Console.ReadLine();
+
+            Console.WriteLine("Введите Email");
+            _email = Console.ReadLine();
+            Console.WriteLine("Введите номер телефона");
+            _phone = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите код");
+            if (_code=="4455")
+            {
+                Console.WriteLine("Вы зарегистрировались успешно");
+            }
+            Console.ReadLine();
+
+
+            
+
+
+
+            
+        }
+
+       
+    }
+}
+
+    
+
+
+
+
---
+++ SMS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+namespace SMS
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            
+            const string accountSid = "ACe78c1bbd72cc1da74d2277f5d8d9ea97";
+            const string authToken = "bf7612619dca0adb3412474d9cf42e20";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "4455",
+                from: new Twilio.Types.PhoneNumber("+18646332809"),
+                to: new Twilio.Types.PhoneNumber("+77784717809")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+
+        
+    }
+    
+    
+
+
+}
+
